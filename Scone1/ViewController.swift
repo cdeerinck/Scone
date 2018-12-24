bde1bb51b70ebfff040ae039ef8d417e61b5c2b1
Chuck Deerinck <cjd9@me.com>
2018-12-23 19:31:20 -0800

    Initial Commit

---
+++ Scone1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scone1
+//
+//  Created by Chuck Deerinck on 12/23/18.
+//  Copyright © 2018 Chuck Deerinck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
